80d8239098ef54374f8e47c35d9ea9dfe94b4a27
mattyrob <mattyrobuk@gmail.com>
2019-07-06 15:41:27 +0100

    Fix warning on some PHP versions

--- classes/class-s2-core.php
+++ classes/class-s2-core.php
@@ -1026,7 +1026,7 @@ class S2_Core {
 		if ( true === apply_filters( 's2_validate_email_with_dns', true ) ) {
 			$domain = explode( '@', $email, 2 );
 			if ( function_exists( 'idn_to_ascii' ) ) {
-				$check_domain = idn_to_ascii( $domain[1] );
+				$check_domain = idn_to_ascii( $domain[1], IDNA_NONTRANSITIONAL_TO_ASCII, INTL_IDNA_VARIANT_UTS46 );
 			} else {
 				$check_domain = $domain[1];
 			}
