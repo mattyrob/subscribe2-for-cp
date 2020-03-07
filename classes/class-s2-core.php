39b4218e40fdcb9ad75dbb4262b8d698ef9841e6
mattyrob <mattyrobuk@gmail.com>
2020-03-07 16:38:00 +0000

    Changes for exclusive ClassicPress support
    - Remove Gutenberg Code
    - Refactor code to be exclusive for ClassicPress
    - Update POT file

--- classes/class-s2-core.php
+++ classes/class-s2-core.php
@@ -1936,17 +1936,6 @@ class S2_Core {
 			$this->block_editor = true;
 		}
 
-		// Compatibility with Fusion Builder
-		if ( is_plugin_active( 'fusion-builder/fusion-builder.php' ) && ! isset( $_GET['gutenberg-editor'] ) ) {
-			$this->block_editor = false;
-		}
-
-		if ( true === $this->block_editor ) {
-			require_once S2PATH . 'classes/class-s2-block-editor.php';
-			global $mysubscribe2_block_editor;
-			$mysubscribe2_block_editor = new S2_Block_Editor();
-		}
-
 		// Add actions specific to admin or frontend
 		if ( is_admin() ) {
 			//add menu, authoring and category admin actions
